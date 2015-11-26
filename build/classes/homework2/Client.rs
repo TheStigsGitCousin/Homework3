133e16c852eb9972aafbdd0b1e79ef3f51ba8afd
TheStigsGitCousin <david.i.fagerlund@gmail.com>
2015-11-26 10:55:47 +0100

    Initital

---
+++ build/classes/homework2/Client.rs
@@ -0,0 +1,3 @@
+se.kth.id2212.ex2.bankrmi.Client$CommandName
+se.kth.id2212.ex2.bankrmi.Client
+se.kth.id2212.ex2.bankrmi.Client$Command
---
+++ src/homework2/bank/RejectedException.java
@@ -0,0 +1,9 @@
+package homework2.bank;
+
+final public class RejectedException extends Exception {
+    private static final long serialVersionUID = -314439670131687936L;
+
+    public RejectedException(String reason) {
+        super(reason);
+    }
+}
